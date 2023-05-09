fixed accuracy calculation

fastNfair/data/unfair_2d.py
@@ -90,7 +90,7 @@ def visualize_unfair_data(x, y, s, net=None, show_orig=True, domain=(0, 1, 0, 1)
     plt.contourf(x_grid, y_grid, c_grid.reshape(x_grid.shape), alpha=0.25, cmap=cmap)
 
     # create classifiers
-    t = torch.linspace(0, 1, 100)
+    t = torch.linspace(domain[0], domain[1], 100)
 
     # original line
     if show_orig or net is None:

fastNfair/objective_functions/logistic.py
@@ -4,8 +4,9 @@ from fastNfair.objective_functions import ObjectiveFunction
 
 class ObjectiveFunctionLogisticRegression(ObjectiveFunction):
 
-    def __init__(self, net):
+    def __init__(self, net, threshold=0.0):
         super(ObjectiveFunctionLogisticRegression, self).__init__(net)
+        self.threshold = threshold
 
     def forward(self, x, y, do_gradient=False, do_Hessian=False):
         n = x.shape[0]
@@ -13,7 +14,7 @@ class ObjectiveFunctionLogisticRegression(ObjectiveFunction):
         f, df, d2f = self.net(x, do_gradient=do_gradient, do_Hessian=do_Hessian)
 
         y = y.view(f.shape)
-        info = {'num_correct': torch.sum((f > 0.5) == (y > 0.5)).item()}
+        info = {'num_correct': torch.sum((f > self.threshold) == y).item()}
 
         beta = 1.0 / n
         p = torch.sigmoid(f)
@@ -45,7 +46,6 @@ class ObjectiveFunctionLogisticRegression(ObjectiveFunction):
                                        @ df.permute(0, 2, 1).unsqueeze(1))).permute(0, 2, 3, 1))
 
         return loss, dloss, d2loss, info
-
 
 
 if __name__ == '__main__':

fastNfair/training/adversarial_training.py
@@ -77,16 +77,8 @@ def train_one_epoch(fctn, optimizer, x, y, s, regularizer=None, batch_size=32, r
             loss, _, _, info = fctn(xb, yb)
         else:
             with torch.no_grad():
-                # opt = TrustRegionNewton(max_iter=5)
-                # delta = torch.randn_like(xb)
-                # delta /= delta.norm()
-                # # # delta *= 1e-8
-                # # # delta /= torch.norm(delta)  # TODO: don't want ||delta|| = 1 for classification
-                # fctn_perturb = ObjectiveFunctionPerturbation(fctn, xb, yb)
-                # # delta, info = opt.solve(fctn_perturb, delta, verbose=True, log_interval=1)
                 opt = TrustRegionSubproblem(max_iter=100, per_sample=True)
                 fctn_max = ObjectiveFunctionMaximize(fctn, yb)
-                # delta, _ = opt.solve(fctn_perturb, delta, radius)
                 xt, info = opt.solve(fctn_max, xb, radius)
 
             optimizer.zero_grad()
@@ -96,11 +88,7 @@ def train_one_epoch(fctn, optimizer, x, y, s, regularizer=None, batch_size=32, r
         if regularizer is not None:
             reg = regularizer(fctn, xb, yb, sb)[0]
             loss = loss + reg
-            # sb = s[idxb]
-            # reg0 = fctn(xb[sb == 0], yb[sb == 0])[0]
-            # reg1 = fctn(xb[sb == 1], yb[sb == 1])[0]
-            # alpha = 1e2
-            # loss = loss + alpha * (0.5 * (reg0 - reg1) ** 2)
+
         # update network weights
         loss.backward()
         optimizer.step()
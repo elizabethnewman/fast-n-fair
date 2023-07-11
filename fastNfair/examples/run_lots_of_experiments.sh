a87771efcdf05ab53babc1e6f6455115f74d6dcb
Hung Anh Vu <123837425+LuxOnly101@users.noreply.github.com>
2023-07-11 11:53:06 -0400

    Update run_lots_of_experiments.sh

--- fastNfair/examples/run_lots_of_experiments.sh
+++ fastNfair/examples/run_lots_of_experiments.sh
@@ -2,7 +2,17 @@
 export PATH=$PATH:/c/Users/Hung/AppData/Local/Microsoft/WindowsApps/python3
 
 
-for r in $(seq 0.1 0.1 0.8);
+for r in $(seq 0.1 0.01 0.2);
 do
-  python3 ex_unfair_2d.py --save --robust --radius $r
+  python ex_unfair_2d.py --save --robust --radius $r --robustOptimizer 'trust' --verbose
+done
+
+for r in $(seq 0.1 0.01 0.2);
+do
+  python ex_unfair_2d.py --save --robust --radius $r --robustOptimizer 'pgd' --verbose
+done
+
+for r in $(seq 0.1 0.01 0.2);
+do
+  python ex_unfair_2d.py --save --robust --radius $r --robustOptimizer 'rand' --verbose
 done

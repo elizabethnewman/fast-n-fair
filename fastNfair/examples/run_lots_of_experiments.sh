2c1718f9f56e80be9f2d98cb960444f848fce462
Allen Minch <122943737+allenjminch@users.noreply.github.com>
2023-07-12 10:48:14 -0400

    Update run_lots_of_experiments.sh

--- fastNfair/examples/run_lots_of_experiments.sh
+++ fastNfair/examples/run_lots_of_experiments.sh
@@ -22,10 +22,3 @@ python ex_lsat.py --save --verbose
 # python ex_adult.py --save --verbose
 
 
-export PATH=$PATH:/c/Users/Hung/AppData/Local/Microsoft/WindowsApps/python3
-
-
-for r in $(seq 0.1 0.1 0.8);
-do
-  python3 ex_unfair_2d.py --save --robust --radius $r
-done
